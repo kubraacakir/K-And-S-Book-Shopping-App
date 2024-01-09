3557b7a4b70a0a61a397cf30bcf310e71a5590f3
kubraacakir <kubra.ckr.06@gmail.com>
2024-01-10 00:53:55 +0300

    be codes update

--- Users/src/main/java/com/example/users/business/abstracts/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.users.business.abstracts;
-
-import com.example.users.entities.concretes.User;
-
-public interface UserService {
-    void saveUser(User user);
-
-    User getUser(User user) throws Exception;
-}
